e53b8488c712602a4a822ab48bc4be807cae042f
jackiesafari <107513244+jackiesafari@users.noreply.github.com>
2023-04-26 01:22:59 -0400

    updated contract number

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -4,7 +4,7 @@
 pragma solidity ^0.8.0;
 
 // Switch this to your own contract address once deployed, for bookkeeping!
-// Example Contract Address on Goerli: 0xDBa03676a2fBb6711CB652beF5B7416A53c1421D
+// Example Contract Address on Mumbai: 0xc303D7d294F7207d34aeA34bE772dcBd0A0c2eb8
 
 contract BuyMeACoffee {
     // Event to emit when a Memo is created.
